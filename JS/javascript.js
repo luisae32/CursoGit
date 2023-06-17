6e8405c33384b30edeb737388022d3896ecadd94
Eliana <luisa.e.tobon@gmail.com>
2023-06-16 20:53:36 -0500

    parrafo y tamaño fuente

---
+++ JS/javascript.js
@@ -0,0 +1,3 @@
+Las polillas son insectos del orden de los Lepidópteros, es decir, son mariposas. Existen muchos tipos de polillas, la mayoría son mariposa
+s nocturnas que no generan ningún tipo de problema a las personas. Sin embargo, 
+existen unas pocas especies que se comportan como plagas cuando se introducen en las casas.
